f91457e226899f05a5174d607d963a2593be14d8
DmitriiObushko <105298559+DmitriiObushko@users.noreply.github.com>
2023-05-20 15:43:30 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,22 +1,23 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+
 class Train {
  private:
   struct Cage {
     Cage():light(false), next(nullptr), prev(nullptr) {}
-    bool light; // состояние лампочки
+    bool light;
     Cage *next;
     Cage *prev;
   };
-  int countOp; 
+  int countOp;
   int amount;
-  Cage* head; 
+  Cage* head;
   Cage* tail;
  public:
   Train();
-  void addCage(bool light); 
-  int getLength();          
-  int getOpCount();         
+  void addCage(bool light);
+  int getLength(); 
+  int getOpCount();    
 };
 #endif  // INCLUDE_TRAIN_H_
